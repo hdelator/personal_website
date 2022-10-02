06edf05017d90536cd98641a6873930880be3b77
hdelator <htorrecasado@gmail.com>
2022-10-02 15:58:04 +0200

    Added AWS URL to allowed hosts

--- personal_website/settings.py
+++ personal_website/settings.py
@@ -23,7 +23,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['http://www.hdelatorre.com/']
+ALLOWED_HOSTS = ['eb-django-app-dev.elasticbeanstalk.com']
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
